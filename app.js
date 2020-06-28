4c5c614749724886c42e43fe2b5673b909ab99b4
Andrew Li <atl15c02@gmail.com>
2020-06-28 18:25:43 -0500

    stores user input into variable

---
+++ app.js
@@ -0,0 +1,22 @@
+let board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+document.getElementById('button').addEventListener("click", event => {
+  let squareID = 0
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 9; x++) {
+      ++squareID;
+      board[y][x] = Number(document.getElementById(squareID).value)
+    }
+  }
+  let xhr =
+    console.log(board);
+})
